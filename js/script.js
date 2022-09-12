1efbd14d671a8ba4433046df7ebfd2f72893350c
Dilshod <dilshodxdxd@gmail.com>
2022-09-13 01:41:58 +0500

    My first portfolio

---
+++ js/script.js
@@ -0,0 +1,103 @@
+// const array = {
+//     html: {
+//         name: "HTML5",
+//         procent: 95,
+//     },
+//     css: {
+//         name: "CSS3",
+//         procent: 85,
+//     },
+//     js: {
+//         name: "Java Script",
+//         procent: 70
+//     },
+//     bs: {
+//         name: "Bootstrap",
+//         procent: 80
+//     },
+//     git: {
+//         name: "Gir and Github",
+//         procent: 50
+//     },
+//     sass: {
+//         name: "Sass",
+//         procent: 65
+//     },
+//     gulp: {
+//         name: "Gulp",
+//         procent: 60
+//     },
+//     pugg: {
+//         name: "Pug",
+//         procent: 65
+//     },
+//     wodpress: {
+//         name: "Wodpress",
+//         procent: 70,
+//     },
+// };
+// for (let i = 0; i < array.length; i++) {
+//     const item = array[i];
+//     console.log(item);
+// }
+
+// let funkBind = funk.bind(array.html);
+
+// function funk() {
+//     console.log(this.name);
+//     console.log(this.procent);
+// };
+
+// funkBind();
+
+
+// let circle = document.querySelector(".circle-container__progress");
+// circle.setAttribute('style', `stroke-dashoffset: ${git.procent};`);
+
+
+// console.log(circle);
+
+
+// const html = {
+//     name: "HTML5",
+//     procent: 95,
+//     bind: funk.bind(this),
+//     call: funk()
+// };
+
+// function funk(){ 
+//     console.log(this.name)
+// }
+
+// const css = {
+//     name: "CSS3",
+//     procent: 85,
+// };
+// const js = {
+//     name: "Java Script",
+//     procent: 70,
+// };
+// const bs = {
+//     name: "Bootstrap",
+//     procent: 80,
+// };
+// const git = {
+//     name: "Gir and Github",
+//     procent: 50,
+// };
+// const sass = {
+//     name: "Sass",
+//     procent: 65,
+// };
+// const gulp = {
+//     name: "Gulp",
+//     procent: 60,
+// };
+// const pugg = {
+//     name: "Pug",
+//     procent: 65,
+// };
+// const wodpress = {
+//     name: "Wodpress",
+//     procent: 70,
+// };
